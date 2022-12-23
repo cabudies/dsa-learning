ab89462d0c23db463adb58306d1de14eb693b500
Gurjas Singh <gurjassingh92@gmail.com>
2022-12-23 13:52:40 +0530

    working on stair case

--- problems/staircase.py
+++ problems/staircase.py
@@ -1,15 +1,22 @@
 from sys import setrecursionlimit
 setrecursionlimit(11000)
 
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n-1) + fib(n-2) + fib(n-3)
+# def fib(n):
+#     if n <= 1:
+#         return n
+#     return fib(n-1) + fib(n-2) + fib(n-3)
 
 def stair_case(steps):
-    return fib(steps+1)
+    if (steps == 0 ):
+        return 1
+    elif (steps < 0):
+        return 0
+ 
+    else:
+        return stair_case(steps - 3) + stair_case(steps - 2) + stair_case(steps - 1)
 
 # value = input()
-steps = 4 # 7
+# steps = 4 # 7
+steps = 5 # 13
 result = stair_case(steps)
 print(result)
