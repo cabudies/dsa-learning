8945ec0a111c370472e9c0cdac8dd52b20a2922d
Gurjas Singh <gurjassingh92@gmail.com>
2023-01-09 16:21:21 +0530

    working on stack

---
+++ stack/learning/2.stack_using_linked_list.py
@@ -0,0 +1,24 @@
+#inbuilt stack as list
+s=[1,2,3]
+s.append(4)
+s.append(5)
+print(s.pop())
+print(s.pop())
+
+#inbuilt queue
+import queue
+q=queue.Queue()
+q.put(1)
+q.put(2)
+q.put(3)
+q.put(4)
+while not q.empty():
+    print(q.get())
+
+import queue
+q=queue.LifoQueue()
+q.put(1)
+q.put(2)
+q.put(3)
+while not q.empty():
+    print(q.get())
