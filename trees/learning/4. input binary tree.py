df85a6abfa7a714bebef7b6db449415c0b5fe8c9
Gurjas Singh <gurjassingh92@gmail.com>
2023-03-01 18:40:06 +0530

    input binary tree

---
+++ trees/learning/4. input binary tree.py
@@ -0,0 +1,49 @@
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def print_tree(root):
+    if root == None:
+        return
+    print(root.data)
+    print_tree(root.left)
+    print_tree(root.right)
+
+
+def print_tree_detailed(root):
+    if root == None:
+        return
+    print(root.data, end=": ")
+    if root.left:
+        print("L", root.left.data, end=", ")
+    if root.right:
+        print("R", root.right.data, end="")
+    print()
+    print_tree_detailed(root.left)
+    print_tree_detailed(root.right)
+
+
+def tree_input():
+    root = int(input("Enter node data: "))
+    if root == -1:
+        return None
+    
+    root = BinaryTreeNode(data=root)
+    left_child = tree_input()
+    right_child = tree_input()
+    root.left = left_child
+    root.right = right_child
+    return root
+
+root_node = tree_input()
+print_tree_detailed(root=root_node)
+
+## 1
+## 4
+## -1
+## -1
+## 5
+## -1
+## -1
